d75c650650aee2ae02ca718af60d8331353c54e2
Yahir-Salmon <yahirama1234@gmail.com>
2026-03-11 09:54:32 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,8 @@
-import "./globals.css"
-
-export const metadata = {
-  title: "Moneduca",
-  description: "Aprende educación financiera"
-}
-
-export default function RootLayout({
-  children
-}:{
-  children: React.ReactNode
-}){
-
-  return(
-    <html lang="es">
+export default function RootLayout({ children }) {
+  return (
+    <html>
       <body>
-
-        <header className="header">
-          <h1>Moneduca</h1>
-        </header>
-
-        <main className="container">
-          {children}
-        </main>
-
+        {children}
       </body>
     </html>
   )
